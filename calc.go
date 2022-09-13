20da3f8919fc4f3ed655345bf0f5e1345d50cc96
leoldding <leo.ld.ding@gmail.com>
2022-09-13 14:05:39 -0400

    started equation calculator

--- calc.go
+++ calc.go
@@ -53,26 +53,42 @@ func simple() {
 func equation() {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	fmt.Print("Only linear equations.\nOnly use a single type of variable.\nOnly numbers and [+ - =].\n")
+	fmt.Print("Only linear equations.\nOnly use a single type of variable.\nOnly numbers and [+ =].\n")
 	fmt.Print("Enter equation: ")
 	scanner.Scan()
 
+	variable := ""
 	left := true
+	leftNum := 0
+	rightNum := 0
+	curNum := ""
+	waitingForNum := true
+	operation := ""	// var num
 
 	for _, char := range strings.ToLower(scanner.Text()){
-		switch char {
-		case 43:
+		switch {
+		case char >= 97 && char <= 122: // letters
+			if variable != "" && variable != string(char) {
+				fmt.Println("Variable already set to " + variable)
+			}
+			variable = string(char)
+		case char >= 48 && char <= 57: // digits
 
-		case 45:
+		case char == 43: // plus sign
 
-		case 61:
+		case char == 61: // equal sign
 			if left {
 				left = false
 			} else {
 				fmt.Println("Not an equation (too many ='s).")
 				return
 			}
-		case 32:
+			if waitingForNum {
+				fmt.Print(string(char))
+				fmt.Println("is not a number!")
+				return
+			}
+		case char == 32: // space
 			continue
 		default:
 			fmt.Print("Invalid value: ")
